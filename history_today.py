d829e5e475b626c1123e6e824de2e084e6893da3
dengyh <dengyh9009@126.com>
2022-04-05 17:43:45 +0800

    Initial commit

---
+++ history_today.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Time: 2022/4/5 16:05
+# Author: Gavin
+import requests
+import datetime
+
+# class historyToday:
+#     def history_today(self, date):
+now_mouth = datetime.datetime.now().strftime('%m').lstrip("0")
+now_day = datetime.datetime.now().strftime('%d').lstrip("0")
+now_date = now_mouth + "/" + now_day
+
+
+url = "http://v.juhe.cn/todayOnhistory/queryEvent.php"
+data = {"key": "acfcb3484c56ee390e359f9df121b2a4", "date": now_date}
+res = requests.get(url, data)
+print(res.json())
